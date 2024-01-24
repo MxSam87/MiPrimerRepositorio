c24eae05065796d0846710cd8a65242972b536ec
samuelinm3X <sami.688.3307@gmail.com>
2024-01-24 16:31:55 -0500

    Se actualiza proyecto completo

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 //Área de definición de variables.
-let numeroSecreto=6;
-
-//El método alert muestra un popup en el navegador con el mensaje que nosotros le ingresemos.
-alert("Bienvenido al juego de... ¡Adivina el número secreto!");
-
 /*Es mejor ser explícito para declarar las variables que se utilizarán, a modo que se pueda identificar para qué se utiliza esa variable.
-  El método prompt nos ayuda a obtener un dato ingresado por el usuario desde el teclado.
+  Además es importante inicializar las variables.
 */
-let numeroDeUsuario = prompt("Me proporcionas un número entre 1 y 10 por favor:");
+let numeroSecreto=4;
+let numeroDeUsuario=0;
+let intentos=1;
+let palabraVeces="vez";
 
-//Es recomendable después de terminar el código y ver que sea funcional eliminar cualquier console.log, ya que son las buenas prácticas de programación. Si no se borra console.log() puede ocasionar problemas cuando el proyecto se lance a producción.
-console.log(numeroDeUsuario);
+while(numeroDeUsuario!=numeroSecreto){
+    //El método prompt nos ayuda a obtener un dato ingresado por el usuario desde el teclado.  
+    let numeroDeUsuario = prompt("Me proporcionas un número entre 1 y 10 por favor:");
 
-/*Cuando hay un "=" es para asignar valores, el doble igual "==" es para comparar valores como en el caso de la siguiente condición IF.
-  Este código compara el valor del número ingresado con el número secreto.
-*/
-
-if(numeroDeUsuario==numeroSecreto){
-    alert("Acertaste, el número que ingresaste es igual al número secreto");
-}else{
-    alert("No has acertado");
+    //Es recomendable después de terminar el código y ver que sea funcional eliminar cualquier console.log, ya que son las buenas prácticas de programación. Si no se borra console.log() puede ocasionar problemas cuando el proyecto se lance a producción.
+    console.log(numeroDeUsuario);
+    
+    /*Cuando hay un "=" es para asignar valores, el doble igual "==" es para comparar valores como en el caso de la siguiente condición IF.
+      Este código compara el valor del número ingresado con el número secreto.
+    */
+    
+    if(numeroDeUsuario==numeroSecreto){
+        /*En esta línea de código se comienza a usar el recurso de Template String o Interpolación que lleva la siguiente estructura.
+          ${variablequecontienedato}
+          Además para su implementación debemos utilizar las comillas invertidas para abrir y cerrar (`Cualquier ${numero} de segundos`)
+        */
+        alert(`¡Acertaste!, el número es: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraVeces}`);
+    }else{
+      //Evaluando si el número es mayor o menor en un if anidado que dará pistas al usuario para poder acertar al número secreto.
+      if(numeroDeUsuario>numeroSecreto){
+        alert("El número secreto es menor");
+      }else{
+        alert("El número secreto es mayor");
+      }
+      //El contador se incrementa cuando no se acerta.
+      intentos=intentos+1;
+      palabraVeces="veces";
+        //alert("No has acertado");
+    }
 }
