c6748878c24d9e86ae84c752cd2df80dd6e144d3
samuelinm3X <sami.688.3307@gmail.com>
2024-01-24 12:04:11 -0500

    Se agregan comentarios a las primeras dos líneas de código del archivo app.js

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-alert("Mi primer código en GitHub");//el método alert muestra un popup en el navegador con el mensaje que nosotros le ingresemos.
-prompt("Me proporcionas un número por favor:");//el método prompt nos ayuda a obtener un dato ingresado por el usuario desde el teclado.
+alert("Mi primer código en GitHub");//El método alert muestra un popup en el navegador con el mensaje que nosotros le ingresemos.
+prompt("Me proporcionas un número por favor:");//El método prompt nos ayuda a obtener un dato ingresado por el usuario desde el teclado.
