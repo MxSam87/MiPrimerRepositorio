c3f68b466bbd2f2797ad0364ec14d0fdb2c7367c
samuelinm3X <sami.688.3307@gmail.com>
2024-01-24 12:01:28 -0500

    Se modificó un error en los comentarios del archivo HTML

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-alert("Mi primer código en GitHub");
-console.log("2");
+alert("Mi primer código en GitHub");//el método alert muestra un popup en el navegador con el mensaje que nosotros le ingresemos.
+prompt("Me proporcionas un número por favor:");//el método prompt nos ayuda a obtener un dato ingresado por el usuario desde el teclado.
