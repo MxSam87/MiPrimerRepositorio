13365920be50db9c4aa3aa7c4b9d2e9f1148a2e9
samuelinm3X <sami.688.3307@gmail.com>
2024-01-24 17:03:46 -0500

    Se actualiza el archivo JS con bucles y condicionales

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let numeroSecreto=4;
 let numeroDeUsuario=0;
 let intentos=1;
 let palabraVeces="vez";
+let maximosIntentos=3;
 
 while(numeroDeUsuario!=numeroSecreto){
     //El método prompt nos ayuda a obtener un dato ingresado por el usuario desde el teclado.  
@@ -22,8 +23,11 @@ while(numeroDeUsuario!=numeroSecreto){
         /*En esta línea de código se comienza a usar el recurso de Template String o Interpolación que lleva la siguiente estructura.
           ${variablequecontienedato}
           Además para su implementación debemos utilizar las comillas invertidas para abrir y cerrar (`Cualquier ${numero} de segundos`)
+          Otra función muy útil es que cuando utilizamos los Template String podemos incluir código Javascript como por ejemplo
+          se incluye ${intentos==1 ? "vez" : "veces"} esta parte del código hace una evaluación como si se tratase de un if pero más compacto
+          evalua de la siguiente manera: si intentos es igual a 1 entonces imprimirá ves, pero si intentos es mayor que 1 entonces imprimirá veces.
         */
-        alert(`¡Acertaste!, el número es: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`¡Acertaste!, el número es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? "vez" : "veces"}`);
     }else{
       //Evaluando si el número es mayor o menor en un if anidado que dará pistas al usuario para poder acertar al número secreto.
       if(numeroDeUsuario>numeroSecreto){
@@ -31,9 +35,13 @@ while(numeroDeUsuario!=numeroSecreto){
       }else{
         alert("El número secreto es mayor");
       }
-      //El contador se incrementa cuando no se acerta.
-      intentos=intentos+1;
+      //El contador se incrementa cuando no se acerta. Otras formas de incrementar contadores: intentos=intentos+1 o intentos+=1.
+      intentos++;
       palabraVeces="veces";
+      if (intentos>maximosIntentos){
+        alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+        break;
+      }
         //alert("No has acertado");
     }
 }
